8ed8036f7e9f3f44d720333f2636909cfc918724
abddulrahmaanHussein <a.hussein@fis.ps>
2021-12-29 15:34:20 +0200

    Add Global style

--- src/Components/Common/Tag.ts
+++ src/Components/Common/Tag.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export const Tag = styled.span``;
+export const Tag = styled.div`
+    border: 1px solid #d1d1d1;
+    border-radius: 4px;
+    color: ${(props) => props.color || '#767676'};
+    cursor: pointer;
+    display: flex;
+    overflow: hidden;
+    transition: background-color .1s ease-in-out;
+
+    &:hover{
+
+    }
+`;
