730982dd214fe89e203754ed834c3db77af6394b
abddulrahmaanHussein <a.hussein@fis.ps>
2021-12-30 16:33:55 +0200

    Add Image List Component

--- src/@Types/photo.ts
+++ src/@Types/photo.ts
@@ -2,8 +2,9 @@ export interface IPhoto {
   id: number;
   width: number;
   height: number;
-  urls: { large: string; regular: string; raw: string; small: string };
+  urls: { large: string; regular: string; raw: string; small: string, full: string, thumb: string };
   color: string | null;
+  alt_description: string;
   user: {
     username: string;
     name: string;
