cd8fdcbc2b1d06d3f61016a09c2494682dc2acaa
reeham_raed <rolla.ghanima@gmail.com>
2021-12-30 03:36:54 +0200

    common Icon component

---
+++ src/Components/Common/IconButton/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { ReactNode } from 'react'
+import { Wrapper } from './style'
+
+interface IconProps {
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    children: ReactNode;
+}
+
+const Icon:React.FC <IconProps> = ({onClick, children})=> {
+    return (
+        <Wrapper onClick={onClick}>
+            {children}
+        </Wrapper>
+    )}
+export default Icon;
+
+
