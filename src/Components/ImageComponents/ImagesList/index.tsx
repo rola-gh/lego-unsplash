2af2cf2cc0c2da34e0ed3726c6909eb52917430d
eman ebrahim <adla48770@gmail.com>
2022-01-02 11:17:18 +0200

    loading infinte scroll

--- src/Components/ImageComponents/ImagesList/index.tsx
+++ src/Components/ImageComponents/ImagesList/index.tsx
@@ -12,24 +12,30 @@ const Index: React.FC<IProps> = ({photos}) => {
         <Container>
             <List>
                 {
-                    photos.length > 0 &&
-                    <>
-                        <div>
-                            <ImageCard photo={photos[0]}/>
-                            <ImageCard photo={photos[5]}/>
-                            <ImageCard photo={photos[3]}/>
-                        </div>
-                        <div>
-                            <ImageCard photo={photos[1]}/>
-                            <ImageCard photo={photos[0]}/>
-                            <ImageCard photo={photos[2]}/>
-                        </div>
-                        <div>
-                            <ImageCard photo={photos[0]}/>
-                            <ImageCard photo={photos[2]}/>
-                            <ImageCard photo={photos[4]}/>
-                        </div>
-                    </>
+                    // photos.length > 0 &&
+                    // <>
+                    //     <div>
+                    //         <ImageCard photo={photos[0]}/>
+                    //         <ImageCard photo={photos[5]}/>
+                    //         <ImageCard photo={photos[3]}/>
+                    //     </div>
+                    //     <div>
+                    //         <ImageCard photo={photos[1]}/>
+                    //         <ImageCard photo={photos[0]}/>
+                    //         <ImageCard photo={photos[2]}/>
+                    //     </div>
+                    //     <div>
+                    //         <ImageCard photo={photos[0]}/>
+                    //         <ImageCard photo={photos[2]}/>
+                    //         <ImageCard photo={photos[4]}/>
+                    //     </div>
+                    // </>
+                    photos && photos.map((photo,index) => 
+                       ( <div key={index}> 
+                            <ImageCard photo={photo}/>
+
+                        </div>)
+                    )
                 }
                 <Loading />
             </List>
--- src/Views/Home/index.tsx
+++ src/Views/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPhotos } from "../../Redux/AllPhotos/Actions";
 import SearchContainer from "../../Components/Common/SearchContainer";
@@ -7,13 +7,25 @@ import { AppState } from "../../Redux/store";
 
 const Index = () => {
   const dispatch = useDispatch();
-
+const [page,setPage] = useState<number>(1)
   const {
     AllPhotos: { photos },
   } = useSelector((state: AppState) => state);
 
+  const infiniteScroll = () => {
+
+    let Height = window.innerHeight + window.scrollY;
+    let bottom = document.documentElement.offsetHeight;
+    if (Height >= bottom) {
+      setPage(page + 1)
+      dispatch(getAllPhotos(1));
+
+    }
+  }
   useEffect(() => {
-    dispatch(getAllPhotos(1));
+    dispatch(getAllPhotos(10));
+    window.addEventListener('scroll', infiniteScroll)
+    return ()=> window.removeEventListener('scroll',infiniteScroll)
   }, [dispatch]);
 
   return (
